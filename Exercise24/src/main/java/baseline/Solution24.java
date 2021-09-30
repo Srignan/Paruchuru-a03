9ba5a2e906b6589a177469588ea03927638ce930
Srignan <Srignan7@gmail.com>
2021-09-30 13:04:30 -0400

    24 Complete

--- Exercise24/src/main/java/baseline/Solution24.java
+++ Exercise24/src/main/java/baseline/Solution24.java
@@ -4,22 +4,43 @@
  */
 
 package baseline;
-/*
-
- */
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution24 {
     // Create boolean isAnagram function; static boolean isAnagram(String 1, String 2)
-
+    static boolean isAnagram(String A, String B) {
         // Make sure letter case does not matter; 1.toLowerCase = 1, 2.toLowerCase = 2
-
+        A = A.toLowerCase();
+        B = B.toLowerCase();
         //boolean f = false; serves as an else statement so if conditions aren't met, it doesn't pass
-
+        boolean f = false;
         //New variable character arrays to keep track of variables, Array.sort to make the words have
         //the same order when checking if they are equal. ex. a v j a & j a v a would both sort to a a j v
+        char[] c = A.toCharArray();
+        Arrays.sort(c);
+        char[] d = B.toCharArray();
+        Arrays.sort(d);
+        String a = new String(c);
+        String b = new String(d);
+        // if a.equals(b), then boolean is proven true
+        if (a.equals(b)) {
+            f = true;
+        }
+        return f;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
 
-        // if 1.equals(2), then boolean is proven true
-
-        //print result, 1 + " and " + 2 + " are anagrams"
+        System.out.println("Enter two strings and I'll tell you if they are anagrams: ");
+        System.out.print("Enter the first string: ");
+        String A=sc.nextLine();
+        //print result, a + " and " + b + " are anagrams"
+        System.out.print("Enter the second string: ");
+        String B=sc.nextLine();
+        if(isAnagram(A,B))
+            System.out.println("\"" + A + "\" and \"" + B + "\" are anagrams.");
+        else
+            System.out.println("\"" + A + "\" and \"" + B + "\" are not anagrams.");
+    }
+}
