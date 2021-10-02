dc7801f927151179de7a89d92e88d606809ec274
Srignan <Srignan7@gmail.com>
2021-10-02 00:16:46 -0400

    29 Complete

--- Exercise29/src/main/java/baseline/solution29.java
+++ Exercise29/src/main/java/baseline/solution29.java
@@ -1,23 +1,45 @@
 package baseline;
 
 import java.util.Scanner;
-public class solution29 {
+public class solution29{
     //void main (String[] args)
+        public static void main(String[] args)
 
-    //Scanner
-    // int r
-    //boolean flag= false
-    //while(true)
-
-    // try catch
-	/*try { flag = true;
+        //Scanner
+        // int r
+        //boolean flag= false
+        //while(true)
+        // try catch
+{
+    Scanner input=new Scanner(System.in);
+    boolean flag=false;
+    while(true)
+    {
+        /*try { flag = true;
 	system print ("what is the rate of return?)
 	int r = integer nex scan
 	System print ("it will take +(72/r)+ years to double your investment.
 	}
-
 	Catch (NumberFormatException)
 	Catch (ArithmeticException)
-	System print ("Sorry thats not a valid input.")
+	System print ("Sorry that's not a valid input.")
 	*/
+        try
+        {
+            System.out.print("What is the rate of return? ");
+            int r=Integer.parseInt(input.next());
+            System.out.println("It will take "+(72/r)+" years to double your initial investment.");
+            flag=true;
+        }
+        catch(NumberFormatException | ArithmeticException e)
+        {
+            System.out.println("Sorry. That's not a valid input.");
+        }
+        if(flag)
+        {
+            break;
+        }
+
+    }
+}
 }
