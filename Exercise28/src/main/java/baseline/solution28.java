7260cf9278c84da34489e9eb7131907cbef61e0c
Srignan <Srignan7@gmail.com>
2021-10-01 23:21:09 -0400

    28 Start

--- Exercise28/src/main/java/baseline/solution28.java
+++ Exercise28/src/main/java/baseline/solution28.java
@@ -1,4 +1,14 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class solution28 {
+/*
+        total=0
+for (int i = 0; i < 5; i++) {
+        print("Enter a number: ");
+        total += in.nextInt();
+
+        print "The total is +total+."
+*/
 }
