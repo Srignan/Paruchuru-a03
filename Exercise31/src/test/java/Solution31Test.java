80d486124e04cf7c3385d2a00e4bae937ff28095
Srignan <Srignan7@gmail.com>
2021-10-02 14:28:22 -0400

    33 Start

---
+++ Exercise31/src/test/java/Solution31Test.java
@@ -0,0 +1,8 @@
+import baseline.Solution31;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Solution31Test {
+
+
--- Exercise33/src/main/java/baseline/Solution33.java
+++ Exercise33/src/main/java/baseline/Solution33.java
@@ -5,5 +5,22 @@
 
 package baseline;
 
+import java.lang.String;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Solution33 {
+    //void question()
+    //{State scanner
+    // print "Whats your Question?"
+    //scan.nextline}
+
+    //void main (String[] args)
+    // String [] responses = yes, no, maybe, ask again later
+
+    //Call question
+
+    // int randomresponse = random.nextInt
+    // String response = responses[RandomResponse]
+    //print response
 }
