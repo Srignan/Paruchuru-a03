b358d61f1030627f399cf97353eab0c92d90d3c5
Srignan <Srignan7@gmail.com>
2021-10-02 02:19:39 -0400

    30 Start

---
+++ Exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Solution30 {
+    /*
+ Public static void main(string[] args)
+		{for (int i=1; i=12;i++)
+			for (int j=1; j=12; j++)
+			{(print i*j)
+			}
+			print}
+     */
+}
