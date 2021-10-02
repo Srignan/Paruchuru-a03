81a7defc2415a30935140b1e7c3381ae5aec08c4
Srignan <Srignan7@gmail.com>
2021-10-02 02:40:37 -0400

    31 Start

--- Exercise30/src/main/java/baseline/Solution30.java
+++ Exercise30/src/main/java/baseline/Solution30.java
@@ -1,12 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 30 Solution
+ *  Copyright 2021 Srignan Paruchuru
+ */
+
 package baseline;
 
 public class Solution30 {
-    /*
- Public static void main(string[] args)
-		{for (int i=1; i=12;i++)
-			for (int j=1; j=12; j++)
-			{(print i*j)
-			}
-			print}
-     */
+
+    //Public static void main(string[] args)
+    public static void main(String[] args)
+    {
+        //for (int i=1; i<=12;i++)
+        for(int i=1;i<=12;i++)
+        {
+            //for (int j=1; j=12; j++)
+            for(int j=1;j<=12;j++)
+            {
+                //{(print i*j +  )
+                System.out.print((i*j)+"  ");
+            }
+            System.out.println();
+        }
+    }
 }
--- Exercise31/src/main/java/baseline/Solution31.java
+++ Exercise31/src/main/java/baseline/Solution31.java
@@ -1,4 +1,19 @@
-package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 31 Solution
+ *  Copyright 2021 Srignan Paruchuru
+ */
 
+package baseline;
+import java.util.Scanner;
 public class Solution31 {
 }
+//int restingHR, age
+//scanner
+//restingHR = scanner.nextInt
+//age= scanner.nextInt
+
+//system out print ("Resting pulse: " + restingHR + " Age: " + age);
+// int intensity = 55
+// while intensity=<95
+//int target = (220-age) - restingHR)*intensity +restingHR
+//print out (intensity+ "% | "+Target +"bpm")
