fac2b744800b351d8e9a0dd36333263141da8966
Srignan <Srignan7@gmail.com>
2021-10-01 23:50:33 -0400

    29 Start

--- Exercise29/src/main/java/baseline/solution29.java
+++ Exercise29/src/main/java/baseline/solution29.java
@@ -1,4 +1,23 @@
 package baseline;
 
+import java.util.Scanner;
 public class solution29 {
+    //void main (String[] args)
+
+    //Scanner
+    // int r
+    //boolean flag= false
+    //while(true)
+
+    // try catch
+	/*try { flag = true;
+	system print ("what is the rate of return?)
+	int r = integer nex scan
+	System print ("it will take +(72/r)+ years to double your investment.
+	}
+
+	Catch (NumberFormatException)
+	Catch (ArithmeticException)
+	System print ("Sorry thats not a valid input.")
+	*/
 }
