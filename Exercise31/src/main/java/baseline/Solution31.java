3c72dd01f081251f96e9d045c5bc859d3bb5ee0d
Srignan <Srignan7@gmail.com>
2021-10-02 03:13:23 -0400

    31 Complete

--- Exercise31/src/main/java/baseline/Solution31.java
+++ Exercise31/src/main/java/baseline/Solution31.java
@@ -5,15 +5,37 @@
 
 package baseline;
 import java.util.Scanner;
-public class Solution31 {
-}
-//int restingHR, age
-//scanner
-//restingHR = scanner.nextInt
-//age= scanner.nextInt
 
-//system out print ("Resting pulse: " + restingHR + " Age: " + age);
-// int intensity = 55
-// while intensity=<95
-//int target = (220-age) - restingHR)*intensity +restingHR
-//print out (intensity+ "% | "+Target +"bpm")
+public class Solution31 {
+    public static void main(String[] args) {
+
+        //int restingHR, age
+        //scanner
+        //restingHR = scanner.nextInt
+        //age= scanner.nextInt
+
+        int restingHR, age;
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your resting heart rate? ");
+        restingHR = input.nextInt();
+        System.out.print("What is your age? ");
+        age = input.nextInt();
+
+        //system out print ("Resting pulse: " + restingHR + " Age: " + age);
+        System.out.println("Resting Pulse: " + restingHR + " Age: " + age);
+
+        // int intensity = 55; (sample output starts at 55)
+        int intensity = 55;
+        System.out.println("Intensity | Rate ");
+        System.out.println("------------------------");
+
+        // while intensity=<95
+        //int target = ((220-age) - restingHR) *intensity +restingHR
+        //print out (intensity+ "% | "+Target +"bpm")
+        while (intensity <= 95) {
+            double Target = (((220 - age) - restingHR) * intensity/100.00) + restingHR;
+            System.out.println(intensity + "%       | " + String.format("%.0f",Target) + " bpm");
+            intensity += 5;
+        }
+    }
+}
