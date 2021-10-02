f61c8155068fa8b78933d69012333daba10b8b69
Srignan <Srignan7@gmail.com>
2021-10-01 23:23:57 -0400

    28 Complete

--- Exercise28/src/main/java/baseline/solution28.java
+++ Exercise28/src/main/java/baseline/solution28.java
@@ -3,12 +3,21 @@ package baseline;
 import java.util.Scanner;
 
 public class solution28 {
-/*
-        total=0
-for (int i = 0; i < 5; i++) {
-        print("Enter a number: ");
-        total += in.nextInt();
 
-        print "The total is +total+."
-*/
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //total=0
+        int total = 0;
+        //for (int i = 0; i < 5; i++) {
+        //        print("Enter a number: ");
+        //        total += in.nextInt();
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter a number: ");
+            total += in.nextInt();
+        }
+        // print "The total is +total+."
+        System.out.println("The total is " + total + ".");
+    }
 }
+
+
