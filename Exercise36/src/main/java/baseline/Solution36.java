eecb8e3ec045e918ae490cb713b77f383ae8b7d1
Srignan <Srignan7@gmail.com>
2021-10-03 00:45:40 -0400

    36 Complete

--- Exercise36/src/main/java/baseline/Solution36.java
+++ Exercise36/src/main/java/baseline/Solution36.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 36 Solution
+ *  Copyright 2021 Srignan Paruchuru
+ */
+
 package baseline;
 
 import java.lang.String;
@@ -7,26 +12,52 @@ import java.util.stream.Collectors;
 
 public class Solution36
 {
+    public static void main(String[] args)
+    {
         /*
         Public void main (String [] args)
         Scanner input = scanner(system.in);
         ArrayList<int> numbers = new ArrayList<>()
         String entry;
          */
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        String entry;
 
-    /*
-       print "Enter a number: "
-       entry = input.next();
-       if (!entry.equals("done"))
-            { int num = integer.entry
-             numbers.add(Num)}
-        while (!entry.equals("done"))
-    */
+        do
+        {
+            /*
+            print "Enter a number: "
+            entry = input.next();
+                if (!entry.equals("done"))
+                    { int num = integer.entry
+                    numbers.add(Num)}
+              while (!entry.equals("done"))
+             */
+            System.out.println("Enter a number: ");
+            entry = input.next();
 
+            if(!entry.equals("done"))
+            {
+                int num = Integer.parseInt(entry);
+                numbers.add(num);
+
+            }
+        }while(!entry.equals("done"));
 
         /*
         Print statements for each function, call functions for average, max, min, and standard deviation
          */
+        System.out.println("Numbers: "+numbers.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+
+        System.out.println("The average is "+average(numbers));
+
+        System.out.println("The minimum is "+min(numbers));
+
+        System.out.println("The maximum is "+max(numbers));
+
+        System.out.println("The standard deviation is "+String.format("%.2f", std(numbers)));
+    }
 
     /*
     Average function
@@ -38,7 +69,16 @@ public class Solution36
         }
         return total/numbers.size (average formula)
      */
+    public static double average(ArrayList<Integer> numbers)
+    {
+        int total = 0;
 
+        for(int num: numbers)
+        {
+            total += num;
+        }
+        return (double)(total)/numbers.size();
+    }
 
     /*
     Max function
@@ -52,7 +92,18 @@ public class Solution36
         return max
      */
 
+    public static int max(ArrayList<Integer> numbers)
+    {
+        int max= numbers.get(0);
 
+        for(int num: numbers)
+        {
+            if(num > max)
+                max = num;
+        }
+
+        return max;
+    }
 
     /*
     Min function
@@ -65,7 +116,18 @@ public class Solution36
         }
         return min
      */
+    public static int min(ArrayList<Integer> numbers)
+    {
+        int min= numbers.get(0);
 
+        for(int num: numbers)
+        {
+            if(num < min)
+                min = num;
+        }
+
+        return min;
+    }
 
     /*
     standard deviation function
@@ -80,5 +142,16 @@ public class Solution36
     }
 }
  */
+    public static double std(ArrayList<Integer> numbers)
+    {
+        double mean = average(numbers);
 
+        double sum = 0;
+        for(int num: numbers)
+        {
+            sum = sum + Math.pow((num-mean), 2);
+        }
+
+        return Math.sqrt(sum/numbers.size());
+    }
 }
