b75739766f3e031ea54f0e88558d7beab8816cc4
Srignan <Srignan7@gmail.com>
2021-10-02 22:34:55 -0400

    35 Complete

--- Exercise35/src/main/java/baseline/Solution35.java
+++ Exercise35/src/main/java/baseline/Solution35.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Solution35 {
+public class Solution35
+{
     /*
     public static int index(arrayList<string> nameList; for list of names
-    {random random = new Random(); return random.nextInt(nameList.size()); }
+    {Random random = new Random(); return random.nextInt(nameList.size()); }
 
      */
-
+    public static int index(ArrayList<String> nameList)
+    {
+        Random random = new Random();
+        return random.nextInt(nameList.size());
+    }
+    public static void main(String[] args)
+    {
         /*
         Void main
         new scanner input
@@ -19,4 +26,21 @@ public class Solution35 {
         Print "Enter a name: " and scan it
         if name is blank, then end program and print random name
          */
+        Scanner input = new Scanner(System.in);
+        String name;
+        ArrayList<String> nameList = new ArrayList<>();
+
+        do {
+            System.out.println("Enter a name: ");
+            name= input.nextLine();
+
+            if(!name.isBlank() && !name.isEmpty())
+                nameList.add(name);
+
+        }while(!name.isBlank() && !name.isEmpty());
+
+        int randomIndex = index(nameList);
+
+        System.out.println("The winner is... "+nameList.get(randomIndex)+".");
+    }
 }
