f95d404c47050a8b58c82fb8859cb516d1fdc6bf
Srignan <Srignan7@gmail.com>
2021-10-03 19:17:35 -0400

    26 Test Code Complete

--- Exercise26/src/test/java/Solution26Test.java
+++ Exercise26/src/test/java/Solution26Test.java
@@ -2,6 +2,20 @@
  *  UCF COP3330 Fall 2021 Assignment 24 Solution
  *  Copyright 2021 Srignan Paruchuru
  */
+import baseline.Solution26;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Solution26Test {
+    @Test
+    void test()
+    {
+        double expected = 70;
+        double actual = Solution26.calculateMonthsUntilPaidOff(5000,.12,100);
+        double x = Math.ceil(actual);
+        assertEquals(expected, x);
+    }
 }
+
+
