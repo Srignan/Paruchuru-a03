4f9407567d27fc192fa9e35e0072197d2d7c9964
Srignan <Srignan7@gmail.com>
2021-10-03 02:32:49 -0400

    38 Start

--- Exercise38/src/main/java/baseline/Solution38.java
+++ Exercise38/src/main/java/baseline/Solution38.java
@@ -5,5 +5,44 @@
 
 package baseline;
 
+import java.lang.String;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class Solution38 {
+    /*
+      public static Integer[] filterEvenNumbers(String[] list)
+      {
+        ArrayList<Integer> newList = new ArrayList<>();
+            for (String s: list) {
+            int num = integer.parseInt
+            if (num%2 == 0) {
+            newList.add(num)
+            }
+            Integer[] List = new Integer[newList.size()];
+        List = newList.toArray(List);
+
+        return List;
+
+     */
+
+    /*
+    public static void main(String args)
+    {
+    State Scanner
+    Print "Enter a list of numbers, separated by spaces: "
+    Scan to input.nextLine
+
+    String array List = data.split(" ") to filter even numbers
+        Integer[] evenList = filterEvenNumbers(list);
+
+        print("The even numbers are ");
+
+        for(int i=0; i < evenList.length; i++)
+            if(i < evenList.length-1)
+                System.out.print(evenList[i] + " ");
+            else
+                System.out.print(evenList[i] + ".");
+     */
 }
