867b5811222475caf4dbecd1970570e8a45e4c59
Srignan <Srignan7@gmail.com>
2021-10-03 18:43:49 -0400

    25 Test Code Complete

--- Exercise25/src/test/java/Solution25Test/Solution25Test.java
+++ Exercise25/src/test/java/Solution25Test/Solution25Test.java
@@ -9,28 +9,28 @@ class Solution25Test {
 
     @Test
     void passwordValidator1() {
-        String expected = "Very Strong";
+        String expected = "This password is very strong.";
         String actual = strengthofpass.passwordValidator("1337h@xor!");
 
         assertEquals(expected, actual);
     }
     @Test
     void passwordValidator2() {
-        String expected = "Strong";
+        String expected = "This password is strong.";
         String actual = strengthofpass.passwordValidator("abc123xyz");
 
         assertEquals(expected, actual);
     }
     @Test
     void passwordValidator3() {
-        String expected = "Weak";
+        String expected = "This password is weak.";
         String actual = strengthofpass.passwordValidator("abcdef");
 
         assertEquals(expected, actual);
     }
     @Test
     void passwordValidator4() {
-        String expected = "Very Weak";
+        String expected = "This password is very weak.";
         String actual = strengthofpass.passwordValidator("12345");
 
         assertEquals(expected, actual);
