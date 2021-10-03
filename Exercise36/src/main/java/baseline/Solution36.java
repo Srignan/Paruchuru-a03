1ccb68ec12fea29c8803e41df5ec1339399c3cae
Srignan <Srignan7@gmail.com>
2021-10-02 23:55:11 -0400

    36 Start

--- Exercise36/src/main/java/baseline/Solution36.java
+++ Exercise36/src/main/java/baseline/Solution36.java
@@ -1,4 +1,84 @@
 package baseline;
 
-public class Solution36 {
+import java.lang.String;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Solution36
+{
+        /*
+        Public void main (String [] args)
+        Scanner input = scanner(system.in);
+        ArrayList<int> numbers = new ArrayList<>()
+        String entry;
+         */
+
+    /*
+       print "Enter a number: "
+       entry = input.next();
+       if (!entry.equals("done"))
+            { int num = integer.entry
+             numbers.add(Num)}
+        while (!entry.equals("done"))
+    */
+
+
+        /*
+        Print statements for each function, call functions for average, max, min, and standard deviation
+         */
+
+    /*
+    Average function
+    public static double average {
+    int total = 0;
+    for (int num: numbers)
+        {
+            total =+ num;  counts numbers to total  (adds all numbers to total so that you can divide later)
+        }
+        return total/numbers.size (average formula)
+     */
+
+
+    /*
+    Max function
+    public static int max (
+    ArrayList<Integer> numbers)
+    for (int num: numbers)
+        {
+            if num>max
+                max = num; (loops till the largest number)
+        }
+        return max
+     */
+
+
+
+    /*
+    Min function
+    public static int max (
+    ArrayList<Integer> numbers)
+    for (int num: numbers)
+        {
+            if num<min
+                min = num; (loops till the smallest number)
+        }
+        return min
+     */
+
+
+    /*
+    standard deviation function
+    {
+        double mean = average(numbers);
+        double sum = 0;
+        for(int num: numbers)
+        {
+            sum = sum + Math.pow((num-mean), 2);
+        }
+        return Math.sqrt(sum/numbers.size());
+    }
+}
+ */
+
 }
